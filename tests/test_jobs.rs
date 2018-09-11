732482c9c6f0b26aefaa2060d824a2728b8bc9d0
David Huss <dh@widerstandzwecklos.at>
2018-09-11 22:05:02 +0200

    Added Framestep to tests

--- tests/test_jobs.rs
+++ tests/test_jobs.rs
@@ -71,8 +71,10 @@ mod frames{
         println!("{:?}", data);
         let start = data.get("Scene").expect("Failed to unwrap FrameRange.start").start;
         let end = data.get("Scene").expect("Failed to unwrap FrameRange.end").end;
+        let count = data.get("Scene").expect("Failed to unwrap FrameRange.count()").count();
         assert_eq!(1, start);
         assert_eq!(250, end);
+        assert_eq!(249, count);
     }
 
     #[test]
@@ -92,8 +94,32 @@ mod frames{
         println!("{:?}", data);
         let start = data.get("Scene").expect("Failed to unwrap FrameRange.start").start;
         let end = data.get("Scene").expect("Failed to unwrap FrameRange.end").end;
+        let count = data.get("Scene").expect("Failed to unwrap FrameRange.count()").count();
         assert_eq!(0, start);
         assert_eq!(1048574, end);
+        assert_eq!(1048574, count);
     }
 
+    #[test]
+    fn frame_step() {
+        let mut buf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        buf.push("tests");
+        buf.push("resources");
+        buf.push("100_frames_step10.blend");
+        let path = buf.clone().into_os_string().into_string().expect("Unwrapping pathbuf basic failed");
+        let data = match parse_scenes(&path[..]){
+            Ok(s) => Some(s),
+            Err(e) => {
+                println!("{}", e);
+                None
+            }
+        }.expect("Unwrapping of parsed_scenes() failed");
+        println!("{:?}", data);
+        let start = data.get("Scene").expect("Failed to unwrap FrameRange.start").start;
+        let end = data.get("Scene").expect("Failed to unwrap FrameRange.end").end;
+        let count = data.get("Scene").expect("Failed to unwrap FrameRange.count()").count();
+        assert_eq!(0, start);
+        assert_eq!(100, end);
+        assert_eq!(10, count);
+    }
 }
