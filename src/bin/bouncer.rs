f30eced2a5434da05fc978e97d47e9c242b59f3e
David Huss <dh@widerstandzwecklos.at>
2018-09-11 18:27:18 +0200

    Basic working thing

---
+++ src/bin/bouncer.rs
@@ -0,0 +1,68 @@
+#[macro_use] extern crate serde_derive;
+extern crate docopt;
+extern crate colored;
+extern crate bender_bouncer;
+
+use docopt::Docopt;
+use colored::*;
+use bender_bouncer::{check_blend, parse_scenes};
+
+
+const USAGE: &'static str = "
+bender-bouncer
+
+The bouncer verifies wether one or more files are valid blend files and returns 
+basic information like frame count, resolution etc.
+
+Usage:
+  bouncer <blendfiles>...
+  bouncer (-h | --help)
+  bouncer --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    arg_blendfiles: Vec<String>,
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.deserialize())
+                            .unwrap_or_else(|e| e.exit());
+    
+    for name in args.arg_blendfiles.iter(){
+        // Check if we are dealing with a valid blend file
+        let valid = match check_blend(&name[..]){
+            Ok(()) => {
+                let message = format!("{}", name.bold().on_green());
+                println!("{}", message);
+                true
+            },
+            Err(e) => {
+                let message = format!("{} is not a blend file -> {}", name.bold().on_red(), e);
+                println!("{}", message);
+                false
+            },
+        };
+        // If so, try to get Scene Name, Startframe and Endframe from it
+        if valid {
+            match parse_scenes(&name[..]){
+                Ok(s) => {
+                    for (name, frames) in s.iter(){
+                        println!("Scene Name:   {}\nStartframe:   {}\nEndframe:     {}\n", name, frames.start, frames.end);
+                    }
+                },
+                Err(e) => {
+                    println!("Error: Couldn't parse file: {:?}", e);
+                }
+            };
+            
+        }
+
+    }
+
+}
---
+++ src/lib.rs
@@ -0,0 +1,140 @@
+extern crate flate2;
+#[macro_use] extern crate structure;
+
+use std::collections::HashMap;
+use std::io::prelude::*;
+use flate2::read::GzDecoder;
+
+use std::str;
+use std::fs::File;
+use std::io::SeekFrom;
+
+type GenError = Box<std::error::Error>;
+type GenResult<T> = Result<T, GenError>;
+
+
+/// This function returns Ok(()) if the read file is a valid blend file and an
+/// Error if something is wonky
+pub fn check_blend<S>(path: S) -> GenResult<()> where S: Into<String>{
+    let path = path.into();
+    let mut f = File::open(&path[..])?;
+
+    // read up to 7 bytes from the file header
+    let mut head = vec![0u8; 7];
+    f.read_exact(&mut head)?;
+
+    // Check for gzip magic on first two bytes and unpack if needed
+    if &head[0..2] == &[0x1f, 0x8b]{
+        let mut gz = GzDecoder::new(&head[..]);
+        // empty the buffer and overwrite it with the decompressed version
+        let mut head = vec![0u8; 7];
+        gz.read_exact(&mut head)?;
+    }
+
+    // Try to create a valid utf-8 string from head
+    let header_string = str::from_utf8(&head)?;
+    // If it asserts to assert_equal the file in question is a valid Blend file
+    assert_eq!("BLENDER", header_string);
+    Ok(())
+}
+
+/// A FrameRange Struct that holds a start and a endframe
+#[derive(Debug)]
+pub struct FrameRange{
+    pub start: i32,
+    pub end: i32
+}
+
+impl FrameRange{
+    /// Return the number of frames
+    pub fn count(&self) -> i32{
+        self.end - self.start
+    }
+}
+
+/// A HashMap-based Type to hold a Scene with Scene Name and FrameRange
+pub type Scenes = HashMap<String, FrameRange>;
+
+
+
+/// Find the scenes within the blend file specified. This assumes the file is a
+/// valid blend file and will return a Error if this is not the case. If you are
+/// not sure, check for validity with the `bender_bouncer::check_blend()` function
+pub fn parse_scenes<S>(path: S) -> GenResult<Scenes> where S: Into<String>{
+    let path = path.into();
+    let mut f = File::open(&path[..])?;
+
+    // read up to 32 bytes from the file header
+    let mut buf = vec![0u8; 32];
+    f.read_exact(&mut buf)?;
+
+    // Check for gzip magic on first two bytes and unpack if needed
+    if &buf[0..2] == &[0x1f, 0x8b]{
+        let mut gz = GzDecoder::new(&buf[..]);
+        // empty the buffer and overwrite it with the decompressed version
+        let mut buf = vec![0u8; 32];
+        gz.read_exact(&mut buf)?;
+    }
+
+    // 24 for 64 Bit, 20 for 32 Bit
+    let blender_head_size = if buf[7] == 0x2d { 24 } else { 20 };
+    let is_big_endian = buf[8] == 0x56;
+
+    // Go to 12th byte
+    f.seek(SeekFrom::Start(12))?;
+
+    // And read a buffer of 4 bytes
+    let mut utf8buf = vec![0u8; 4];
+    f.read_exact(&mut utf8buf)?;
+    let mut blender_head_unread;
+    let mut scenes = Scenes::new();
+
+    // Loop through the scenes
+    while str::from_utf8(&utf8buf)? == "REND"{
+        blender_head_unread = blender_head_size - 4;
+        // unpack struct here
+        let mut buf = vec![0u8; 4];
+        f.read_exact(&mut buf)?;
+        match is_big_endian { 
+            true => {
+                let s = structure!(">i");
+                s.unpack(buf)?;
+            },
+            false => {
+                let s = structure!("<i");
+                s.unpack(buf)?;
+            }
+        }
+        blender_head_unread -= 4;
+
+        // We don't care about the rest of the struct
+        buf = vec![0u8; blender_head_unread];
+        f.read_exact(&mut buf)?;
+
+        // Now find the rest
+        buf = vec![0u8; 8];
+        f.read_exact(&mut buf)?;
+        // Find Start and endframes
+        let (start, end) = match is_big_endian { 
+            true => {
+                let s = structure!(">2i");
+                s.unpack(buf)?
+            },
+            false => {
+                let s = structure!("<2i");
+                s.unpack(buf)?
+            }
+        };
+
+        // Find Scenename
+        buf = vec![0u8; 64];
+        f.read_exact(&mut buf)?;
+        let scenename = str::from_utf8(&buf)?;
+        scenes.insert(scenename.to_string(), FrameRange{start, end});
+
+        // Read for the next round
+        utf8buf = vec![0u8; 4];
+        f.read_exact(&mut utf8buf)?;
+    }
+    Ok(scenes)
+}
