c1d76169e851ec647ff41f35926e979615d4efee
David Huss <dh@widerstandzwecklos.at>
2018-12-19 15:15:09 +0100

    Removed install scripts in favour of global install scripts

--- uninstall.sh
+++
@@ -1,4 +0,0 @@
-echo 'Starting to remove bender-bouncer\n'
-echo 'Attempting to remove binaries from /usr/bin:'
-sudo rm -f /usr/bin/bender-bouncer-cli
-echo 'Removed bender-bouncer-cli from /usr/bin'
